55a7cbd0dcdd98308ed3b57b80756bba5531f6a1
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2022-03-14 01:14:43 +0800

    updates the server.sh to include steps to setup consul server

---
+++ scripts/server.sh
@@ -0,0 +1,24 @@
+# Download and Install Consul
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install consul
+
+# Modify the default consul.hcl file
+cat > /etc/consul.d/consul.hcl <<- EOF
+data_dir = "/opt/consul"
+
+client_addr = "0.0.0.0"
+
+ui_config{
+  enabled = true
+}
+
+server = true
+
+bind_addr = "0.0.0.0"
+
+bootstrap_expect=1
+EOF
+
+# Start Consul
+sudo systemctl start consul
